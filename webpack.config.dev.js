5b5b1213c9306dc08a09517c325d0f29c7b39d50
aimajoke <aimajoke16@gmail.com>
2022-01-28 15:27:55 -0500

    Confirmando

---
+++ webpack.config.dev.js
@@ -0,0 +1,73 @@
+/** @type {import('webpack').Configuration} */
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const DotEnv = require('dotenv-webpack')
+module.exports = {
+    entry: './src',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.[contenthash].js',
+        assetModuleFilename: 'assets/[hash][ext][query]'
+    },
+    mode: 'development',
+    resolve:{
+        extensions: ['.js'],
+        alias: {
+            '@utils': path.resolve(__dirname, 'src/utils/'),
+            '@templates': path.resolve(__dirname, 'src/templates/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@images': path.resolve(__dirname, 'src/assets/images/'),
+        }
+    },
+    module:{
+        rules:[{
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        },
+        {
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, 
+            'css-loader'],
+        },
+        {
+            test: /\.png$/,
+            type: 'asset/resource',
+            generator:{
+                filename: "assets/images/[name][contenthash][ext]"
+            }
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: "asset/resource",
+            generator: {
+                filename: "assets/fonts/[name][ext]"
+            }
+        }
+    ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "assets/styles/[name].[contenthash].css"
+        }),
+        // new CopyPlugin({
+        //     patterns:[
+        //         {
+        //             from: path.resolve(__dirname, "src", "assets/images"),
+        //             to: "assets/images"
+        //         }
+        //     ]
+        // })
+        new DotEnv()
+    ],
+    
+}
